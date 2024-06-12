0df3d9b64a0c79bee47174ef3b8bd29639fc636f
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-06-12 17:40:28 +0200

    Fix the color highlighting config
    Ignore the lazy filetype because some strings were interpreted as color
    when they are not.
    
    Place the color marker back at the end of the line instead of "inline".

--- lua/plugins/nvim-highlight-colors.lua
+++ lua/plugins/nvim-highlight-colors.lua
@@ -1,10 +1,12 @@
 -- https://github.com/brenoprata10/nvim-highlight-colors
 
 return {
-  'brenoprata10/nvim-highlight-colors',
+  "brenoprata10/nvim-highlight-colors",
   opts = {
     render = "virtual",
-    virtual_symbol = ' ■',
+    virtual_symbol = " ■",
+    virtual_symbol_position = "eol",
+    exclude_filetypes = { "lazy" },
   },
 }
 
