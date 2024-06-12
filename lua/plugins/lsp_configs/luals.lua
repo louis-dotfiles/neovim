e80124099cfccb071bab0b93af815fed7a9fc56b
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-06-12 20:41:56 +0200

    Add Awesome WM lua api completion

--- lua/plugins/lsp_configs/luals.lua
+++ lua/plugins/lsp_configs/luals.lua
@@ -16,14 +16,17 @@ return {
           -- (most likely LuaJIT in the case of Neovim)
           version = "LuaJIT"
         },
-        -- Make the server aware of Neovim runtime files
+        -- Make the server aware of Neovim and Awesome runtime files
         workspace = {
           checkThirdParty = false,
           library = {
-            vim.env.VIMRUNTIME
+            vim.env.VIMRUNTIME,
             -- Depending on the usage, you might want to add additional paths here.
             -- "${3rd}/luv/library"
             -- "${3rd}/busted/library",
+
+            -- Awesome WM.
+            "/usr/share/awesome/lib",
           }
           -- or pull in all of "runtimepath". NOTE: this is a lot slower
           -- library = vim.api.nvim_get_runtime_file("", true)
@@ -31,8 +34,15 @@ return {
       }
     )
   end,
+
+  -- https://luals.github.io/wiki/settings/
   settings = {
-    Lua = {}
+    Lua = {
+      diagnostics = {
+        -- Awesome WM.
+        globals = { "awesome", "client", "mouse", "root", "screen", "tag" }
+      }
+    }
   }
 }
 
