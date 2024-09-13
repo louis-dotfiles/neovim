0cc57e81f76b91569ab2ddac020bc337316dcd19
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-09-13 15:03:19 +0200

    Change nvim surround mappings

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,24 +3,18 @@
 local function make_options()
   return {
     keymaps = {
-      insert = "<C-g>s",
-      insert_line = "<C-g>S",
-      normal = "ys",
-      normal_cur = "yss",
-      normal_line = "yS",
-      normal_cur_line = "ySS",
+      normal = "sa",
       visual = "S",
-      visual_line = "gS",
-      delete = "ds",
-      change = "cs",
-      change_line = "cS",
+      delete = "sd",
+      change = "sc",
     },
   }
 end
 
 return {
   "kylechui/nvim-surround",
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features.
   event = "VeryLazy",
   opts = make_options,
 }
+
