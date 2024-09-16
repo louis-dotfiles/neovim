4920cac92cadcf7ca392e4dd9adb162afefe5209
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-09-16 17:47:04 +0200

    Replace Aerial plugin with Trouble functionality
    Trouble can now provide Aerial's feature.

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,38 @@
 -- https://github.com/folke/trouble.nvim
 
+local last_outlined_buffer = 0
+
+-- Opens the outline if it's closed.
+-- Closes the outline if it's already open for the current buffer.
+-- Refreshes the outline to the current buffer otherwise.
+local function dynamic_symbols_toggle()
+  local trouble = require("trouble")
+  local mode = "symbols"
+
+  local is_open = trouble.is_open(mode)
+  local current_buffer = vim.api.nvim_win_get_buf(0)
+
+  local function open_symbols()
+    last_outlined_buffer = current_buffer
+    trouble.open({
+      mode = mode,
+      pinned = true,
+      focus = false,
+    })
+  end
+
+  if is_open then
+    if last_outlined_buffer == current_buffer then
+      trouble.close(mode)
+    else
+      trouble.close(mode)
+      open_symbols()
+    end
+  else
+    open_symbols()
+  end
+end
+
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -22,6 +55,21 @@ return {
       "<Leader>dc",
       "<cmd>Trouble diagnostics close<cr>",
       desc = "Close diagnostics",
+    },
+    {
+      "<Leader>oo",
+      dynamic_symbols_toggle,
+      desc = "Open",
+    },
+    {
+      "<Leader>oc",
+      "<cmd>Trouble symbols close<cr>",
+      desc = "Close",
+    },
+    {
+      "<Leader>ot",
+      "<cmd>Trouble symbols toggle pinned=true focus=false new=true<cr>",
+      desc = "toggle test",
     },
   },
 }
