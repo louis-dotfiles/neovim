71a9d411b433ee2c502bd4a8d24ccd8e3330e979
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-09-20 14:47:10 +0200

    Add completion mappings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -17,12 +17,16 @@ local function make_config()
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
     },
+    -- :help cmp-mapping
     mapping = cmp.mapping.preset.insert({
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<C-CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<C-q>"] = cmp.mapping.abort(),
+      ["<C-CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+      }),
     }),
     sources = cmp.config.sources(
       {
