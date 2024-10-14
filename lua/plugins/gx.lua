e33b5debcabc098cc091a98829a65ddd97248db1
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-10-14 15:28:28 +0200

    Minor 'gx' plugin conf changes

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -6,6 +6,7 @@ return {
   init = function ()
     vim.g.netrw_nogx = 1 -- disable netrw gx
   end,
+  submodules = false, -- Submodules are required only for tests.
   opts = {
     handler_options = {
       search_engine = "ecosia"
