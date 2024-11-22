1e30598fda7ce1e0f9cb6fd6a19be786196ffb0b
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-11-22 11:59:11 +0100

    Update highlight-colors plugin conf
    Add a keymap to toggle it on/off quickly.
    Change the icon to a circle, to differentiate it from LSP error icons.
    Load the plugin later in the load order.

--- lua/plugins/nvim-highlight-colors.lua
+++ lua/plugins/nvim-highlight-colors.lua
@@ -4,9 +4,19 @@ return {
   "brenoprata10/nvim-highlight-colors",
   opts = {
     render = "virtual",
-    virtual_symbol = " ■",
+    virtual_symbol = " ",
     virtual_symbol_position = "eol",
-    exclude_filetypes = { "lazy" },
+    exclude_filetypes = {
+      "lazy", -- Lots of git hashes that are interpreted as color codes, and no colors expected in the filetype.
+    },
   },
+  event = "VeryLazy",
+  keys = {
+    {
+      "<F4>",
+      "<cmd>HighlightColors Toggle<cr>",
+      desc = "Toogle color highlighting",
+    },
+  }
 }
 
