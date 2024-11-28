94180e4666257d06fd0db5fa3647a2c620543266
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-11-28 10:08:21 +0100

    Update global diagnostic signs definition

--- lua/config.lua
+++ lua/config.lua
@@ -1,17 +1,15 @@
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#change-diagnostic-symbols-in-the-sign-column-gutter
-local signs = {
-  Error = "󰅚 ",
-  Warn = "󰀪 ",
-  Hint = "󰌶 ",
-  Info = " ",
-}
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
 
--- Border for the automatic LSP errors, warnings ... message popups.
+-- :help diangostic-signs
 vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵 ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
   float = { border = "rounded" },
 })
 
