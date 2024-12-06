beb17e37686dc381f68cf0c7ac8ffca756558904
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-12-06 11:00:58 +0100

    Add gitsign keymaps
    Just some simple git actions I would use while editing.
    They are not meant to replace a dedicated git client to solve
    conflicts or whatnot.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,5 +3,28 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {},
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>gp",
+      function () require("gitsigns").preview_hunk() end,
+      desc = "Preview",
+    },
+    {
+      "<leader>gb",
+      function () require("gitsigns").blame_line() end,
+      desc = "Blame",
+    },
+    {
+      "<leader>gr",
+      function () require("gitsigns").reset_hunk() end,
+      desc = "Reset hunk",
+    },
+    {
+      "<leader>gR",
+      function () require("gitsigns").reset_buffer() end,
+      desc = "Reset buffer",
+    },
+  },
 }
 
