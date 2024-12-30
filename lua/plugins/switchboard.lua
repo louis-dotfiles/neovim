03c411549d2fc80757f99b6b0b8ae3995b3c4a44
Louis VINCHON <louis.vinchon.dev@gmail.com>
2024-12-30 16:10:27 +0100

    Update Switchboard config to use builtins

--- lua/plugins/switchboard.lua
+++ lua/plugins/switchboard.lua
@@ -1,71 +1,39 @@
 -- https://github.com/louis-neovim-plugins/switchboard
 
+---@type SwitchboardSwitch
+local color_preview_switch = {
+    label = "Color preview",
+    is_on = function ()
+        return require("nvim-highlight-colors").is_active()
+    end,
+    keymap = {
+        "c",
+        function ()
+            require("nvim-highlight-colors").toggle()
+        end,
+    },
+}
+
+
+---@param _ any
+---@param opts SwitchboardOpts
+local function make_config(_, opts)
+    local builtins = require("switchboard.builtins")
+
+    opts.switches = {
+        color_preview_switch,
+        builtins.switches.make_diagnostics_switch("d"),
+        builtins.switches.make_inlay_hints_switch("h"),
+        builtins.switches.make_relative_line_numbers_switch("r"),
+        builtins.switches.make_line_wrap_switch("w"),
+    }
+
+    require("switchboard").setup(opts)
+end
+
 return {
     "louis-neovim-plugins/switchboard",
-    opts = {
-        switches = {
-            {
-                label = "Inlay hints",
-                is_on = function ()
-                    return vim.lsp.inlay_hint.is_enabled()
-                end,
-                keymap = {
-                    "h",
-                    function ()
-                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-                    end,
-                },
-            },
-            {
-                label = "Diagnostics",
-                is_on = function ()
-                    return vim.diagnostic.is_enabled()
-                end,
-                keymap = {
-                    "d",
-                    function ()
-                        vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-                    end,
-                },
-            },
-            {
-                label = "Relative line numbers",
-                is_on = function ()
-                    return vim.o.relativenumber
-                end,
-                keymap = {
-                    "n",
-                    function ()
-                        vim.o.relativenumber = not vim.o.relativenumber
-                    end,
-                },
-            },
-            {
-                label = "Line wrap",
-                is_on = function ()
-                    return vim.wo.wrap
-                end,
-                keymap = {
-                    "w",
-                    function ()
-                        vim.wo.wrap = not vim.wo.wrap
-                    end,
-                },
-            },
-            {
-                label = "Color preview",
-                is_on = function ()
-                    return require("nvim-highlight-colors").is_active()
-                end,
-                keymap = {
-                    "c",
-                    function ()
-                        require("nvim-highlight-colors").toggle()
-                    end,
-                },
-            },
-        },
-    },
+    config = make_config,
     keys = {
         {
             "<leader>s",
