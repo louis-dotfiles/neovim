f22e78709357efb8b8fd56cd72f8e68f0ca1bba6
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-01-03 17:54:06 +0100

    Tweak smear_cursor
    Stop the animation when it's within "1 block" of the destination.
    
    It had a tendency to "hide" characters close to the destination and it
    was very noticeable in smaller movements, not so much for bigger
    movements.

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -2,6 +2,9 @@
 
 return {
   "sphamba/smear-cursor.nvim",
-  opts = {},
+  -- https://github.com/sphamba/smear-cursor.nvim/blob/main/lua/smear_cursor/config.lua
+  opts = {
+    distance_stop_animating = 1,
+  },
 }
 
