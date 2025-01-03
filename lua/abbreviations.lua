79cc92a77b8456087712d0f50389a46ab75bb208
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-01-03 16:24:14 +0100

    Update abbreviations

--- lua/abbreviations.lua
+++ lua/abbreviations.lua
@@ -1,7 +1,15 @@
 
 
 -- :h will now open in a vertical split.
-vim.cmd("cnoreabbrev h vert help")
-vim.cmd("cnoreabbrev he vert help")
-vim.cmd("cnoreabbrev help vert help")
+vim.keymap.set("ca", "h",    "vert help")
+vim.keymap.set("ca", "he",   "vert help")
+vim.keymap.set("ca", "hel",  "vert help")
+vim.keymap.set("ca", "help", "vert help")
+
+vim.keymap.set("ca", "W",   "w")
+vim.keymap.set("ca", "Wq",  "wq")
+vim.keymap.set("ca", "Wqa", "wqa")
+
+vim.keymap.set("ca", "Q",  "q")
+vim.keymap.set("ca", "Qa", "qa")
 
