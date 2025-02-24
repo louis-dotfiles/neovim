160d5ce900a3394d50fd518cfd189af01c83c179
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-02-24 22:23:32 +0100

    Use github Switchboard by default

--- lua/plugins/statuscolumn.lua
+++ lua/plugins/statuscolumn.lua
@@ -1,8 +1,8 @@
 -- https://github.com/louis-neovim-plugins/statuscolumn
 
 return {
-  -- "louis-neovim-plugins/statuscolumn",
-  dir = "~/Documents/louis/statuscolumn",
+  "louis-neovim-plugins/statuscolumn",
+  -- dir = "~/Documents/louis/statuscolumn",
   dependencies = { "runiq/neovim-throttle-debounce" },
   opts = {},
 }
