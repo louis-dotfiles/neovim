cf2781b77c802fc69e3110801d41e69a0b4c777a
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-04-22 10:50:10 +0200

    Add keymap to cycle through diagnostic display modes.

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -13,10 +13,49 @@ vim.diagnostic.config({
     severity_sort = true,
 })
 
+
+local modes = {
+    "virtual_lines",
+    "virtual_text",
+}
+local active_mode = 1
+---Cycles through diagnostics disaply modes.
+---Virtual text tends to go "offscreen" on longer lines, and cannot be read.
+---Virtual lines are invasive when there are a lot of errors.
+local function cycle_diagnostic_mode()
+    -- print("active mode", active_mode, modes[active_mode])
+    active_mode = math.fmod(active_mode, #modes) + 1
+    -- print("active mode", active_mode, modes[active_mode])
+
+    local config = {}
+    for _, mode in pairs(modes) do
+        config[mode] = false
+    end
+
+    config[modes[active_mode]] = true
+
+    vim.diagnostic.config(config)
+end
+
+
 vim.api.nvim_create_autocmd("LspAttach", {
-  --group = lspgroup,
-  callback = function(event)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, { buffer = event.buf })
-  end
+    --group = lspgroup,
+    callback = function(event)
+        vim.keymap.set(
+            "n",
+            "K",
+            function() vim.lsp.buf.hover({ border = "rounded" }) end,
+            {
+                buffer = event.buf,
+                desc = "Show the LSP definition",
+            }
+        )
+        vim.keymap.set(
+            "n",
+            "<leader>dm",
+            cycle_diagnostic_mode,
+            { desc = "Cycle through diagnostic display modes" }
+        )
+    end
 })
 
