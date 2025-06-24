4461343c886da17659d8633d616b26479334e212
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-06-24 15:50:03 +0200

    Add auto-session plugin

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,17 @@
+-- https://github.com/rmagatti/auto-session
+
+return {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+        suppressed_dirs = {
+            '~/Downloads',
+        },
+        -- log_level = 'debug',
+    }
+}
+
