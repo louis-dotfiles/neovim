b8b65d8294d5861a8f321610d5a6f928fbdbfa20
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-10-08 15:50:48 +0200

    Replace spectre with grug-far
    In my opinion, it's simply more ergonomic with equivalent features.

---
+++ lua/plugins/grug-far.lua
@@ -0,0 +1,14 @@
+-- https://github.com/MagicDuck/grug-far.nvim
+
+return {
+    'MagicDuck/grug-far.nvim',
+    opts = {},
+    keys = {
+        {
+            "<Leader>/",
+            "<cmd>GrugFar<CR>",
+            desc = "Toogle Search and replace.",
+        },
+    },
+}
+
