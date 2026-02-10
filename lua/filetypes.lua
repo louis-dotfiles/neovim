f37c2e55e6682f1e157d30fdf7c82c664a259ef5
Louis VINCHON <louis.vinchon.dev@gmail.com>
2026-02-10 14:24:03 +0100

    Add helm chart filetype.

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -7,14 +7,33 @@ vim.filetype.add({
         -- Give the proper file type to git configuration files.
         [".*git/config"] = "gitconfig",
 
-        -- Give the proper file type to ansible yaml configuration files.
+        -- Give the proper file type to various yaml configuration files.
         -- This depends on the LSP config: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ansiblels
-        [".*ya?ml"] = function (_, _)
-            local ansible_patterns = { "ansible.cfg", "inventory.ini", "inventory.yaml", "inventory.yml" }
-            local ansible_cfg = vim.fs.find(ansible_patterns, { upward = true })
-            -- print(vim.inspect(ansible_cfg))
+        [".*ya?ml"] = function (path, _)
+            -- Check the name of the file.
+            local yaml_patterns = { "gitlab" }
+            for _, pattern in pairs(yaml_patterns) do
+                print(pattern)
+                if string.match(vim.fs.basename(path), pattern) then
+                    return "yaml"
+                end
+            end
 
-            return vim.tbl_isempty(ansible_cfg) and 'yaml' or 'yaml.ansible'
+            -- Check the repository for Helm configuration files.
+            local helm_patterns = { "Chart.yaml", "Chart.lock.yaml" }
+            local helm_cfg = vim.fs.find(helm_patterns, { upward = true, type = "file", path = path })
+            if vim.tbl_count(helm_cfg) > 0 then
+                return 'helm'
+            end
+
+            -- Check the repository for Ansible configuration files.
+            local ansible_patterns = { "ansible.cfg", "inventory.ini", "inventory.yaml", "inventory.yml" }
+            local ansible_cfg = vim.fs.find(ansible_patterns, { upward = true, type = "file", path = path })
+            if vim.tbl_count(ansible_cfg) > 0 then
+                return 'yaml.ansible'
+            end
+
+            return 'yaml'
         end
     },
     filename = {
